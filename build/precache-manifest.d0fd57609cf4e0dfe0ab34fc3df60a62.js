4ef4eae723f6ca019d34da584bbd396efcdb090f
Danilo <danilo.santitto@gmail.com>
2021-01-29 14:27:00 +0100

    Pages upload

---
+++ build/precache-manifest.d0fd57609cf4e0dfe0ab34fc3df60a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b935027bd6d713f6ec55e35a04c863",
+    "url": "/watchnet-project/index.html"
+  },
+  {
+    "revision": "7bf7c114b816910ec37b",
+    "url": "/watchnet-project/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7bf7c114b816910ec37b",
+    "url": "/watchnet-project/static/js/2.c743f95d.chunk.js"
+  },
+  {
+    "revision": "c0a6b5bdaf71e1381cf8",
+    "url": "/watchnet-project/static/js/main.5e472485.chunk.js"
+  },
+  {
+    "revision": "2ee6ce4be406aa2c183a",
+    "url": "/watchnet-project/static/js/runtime~main.d3232275.js"
+  }
+]);
